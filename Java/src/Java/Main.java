39ed0a7e2a64f363038a800e7a5cbe2824d78867
paulovitorarruda <paulovitorparruda@gmail.com>
2025-09-14 21:42:03 -0300

    Aula 2 JAVA
    Orientação a Objetos - Completo #2
    (Atributos)

--- Java/src/Java/Main.java
+++ Java/src/Java/Main.java
@@ -2,8 +2,16 @@ package Java;
 
 public class Main {
 	
+	
+	/*
 	public static void main(String[] args) {
-		Utils.printHelloWorld();
+		Utils utils = new Utils();
+		utils.printHelloWorld();
+	}
+	*/
+	
+	public static void main(String[] args) {
+		System.out.println(new Utils().pegaIdade());
 	}
 
 }
--- Java/src/Java/Utils.java
+++ Java/src/Java/Utils.java
@@ -2,8 +2,25 @@ package Java;
 
 public class Utils {
 	
-	public static void printHelloWorld() {
-		System.out.println("Hello World!");
+	/* ATRIBUTOS:
+	int,double,char,String,float
+	*/
+	private int idade = 21;
+	public double peso = 72;
+	public float peso2 = 72f;
+	public String nome = "Paulo";
+	
+	public void printHelloWorld() {
+		idade = 21;
+		printOutraCoisa();
+	}
+	
+	public int pegaIdade() {
+		return idade;
+	}
+	
+	private void printOutraCoisa() {
+		System.out.println("outra coisa");
 	}
 
 }
